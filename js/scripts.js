a0f81282615f19009bd845f3986e0102fca7834b
James Henager <jameschenager@gmail.com>
2020-06-15 11:07:40 -0700

    add functions for assigning id's
    Co-Authored-By: Nikolas Kososik <nikolas.kososik@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
 // Business Logic for AddressBook ------
 function AddressBook() {
   this.contacts = [];
+  this.currentId = 0;
 }
 
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+AddressBook.prototype.findContact = function(id) {
+  for (let i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i].id == id) {
+      return this.contacts[i]; // return matched id with associated contact (AKA true)
+    }
+  };
+ return false; // signifies no matching id/contact
+}
 
 // Business Logic for Contacts ------
 function Contact (firstName, lastName, phoneNumber) {
